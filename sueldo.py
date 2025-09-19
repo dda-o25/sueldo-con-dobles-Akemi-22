ff8f584c738f237e22c8ed9178b660dea3362ede
github_username <email_address>
2025-09-19 03:35:28 -0600

    primer intento

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,25 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Akemi Clarissa Olvera Arao
+19/09/25
+Calcular el sueldo semanal de un empleado
 """
 
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+horas = int(input("Horas trabajadas esta semana: "))
+tarifa = int(input("Tarifa horaria: "))
+horas_extras = 0
 
 # Proceso
-
+if horas > 48:
+    horas_extras = horas - 48
+    horas = 48
+pago_extra = horas_extras * tarifa * 2
+sueldo = horas * tarifa + pago_extra
 
 # Salidas
-print(salida)
+print("Este es el sueldo $", sueldo)
+print("Trabajó ", horas_extras, " horas extras y esto se le pagó por esas horas extras", pago_extra)
